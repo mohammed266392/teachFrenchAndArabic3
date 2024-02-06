9c2947cf7cb0b384d4a2d0b8bce520cc1fd6187c
raki <mohammed.raki26@gmail.com>
2024-02-06 16:20:57 +0100

    effet swipe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { TextAnimationComponent } from '../compenent-tools/text-animation/text-animation.component';
 import { ReviewComponent } from '../review/review.component';
 import { SolutionPiscineAnimationComponent } from '../compenent-tools/solution-piscine-animation/solution-piscine-animation.component';
-import { SavoirFaire3Component } from '../savoir-faire3/savoir-faire3.component';
 import { Landing4Component } from '../landing4/landing4.component';
+import { SavoirFaire4Component } from '../savoir-faire4/savoir-faire4.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone:true,
-  imports :  [SolutionPiscineAnimationComponent,TextAnimationComponent, Landing4Component, SavoirFaire3Component, ReviewComponent]
+  imports :  [SolutionPiscineAnimationComponent,TextAnimationComponent, Landing4Component, SavoirFaire4Component, ReviewComponent]
 
 })
 export class HomeComponent {
