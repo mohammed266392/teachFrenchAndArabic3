8c96791bc621bb0641416717d504b2d8fcef5856
raki <mohammed.raki26@gmail.com>
2024-02-09 07:46:38 +0100

    templating

--- src/app/savoir-faire5/savoir-faire5.component.ts
+++ src/app/savoir-faire5/savoir-faire5.component.ts
@@ -12,28 +12,19 @@ import { Router } from '@angular/router';
 })
 export class SavoirFaire5Component {
   visible :boolean=false;
-  imageBlock1 = "./../../assets/images/piscine_savoir_faire_v1.jpg"
+  presentiel = "./../../assets/images/presentiel.svg"
+  distanciel = "./../../assets/images/distanciel.svg"
+
 
   blocs : JeVeuxApprendre[] = [
     new JeVeuxApprendre(0,"Je veux apprendre la langue arabe","si vous parlez le français","learn-arabic",
-    ["./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png"]),
+    ["./../../assets/images/france.svg", "./../../assets/images/canada.svg", "./../../assets/images/pays-bas.svg", "./../../assets/images/suisse.svg", "./../../assets/images/belgique.svg"]),
     new JeVeuxApprendre(1,"أريد أن أتعلم اللغة الفرنسية","إذا كنت تتحدث الفرنسية","learn-french",
-    ["./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png","./../../assets/images/flag_french.png"])
+    ["./../../assets/images/emirats.svg","./../../assets/images/koweit.svg","./../../assets/images/qatar.svg", "./../../assets/images/palestine.svg", "./../../assets/images/maroc.svg","./../../assets/images/lybie.svg", "./../../assets/images/algerie.svg","./../../assets/images/arabie-saoudite.svg","./../../assets/images/egypte.svg","./../../assets/images/jordanie.svg","./../../assets/images/liban.svg","./../../assets/images/tunisie.svg"])
 
   ]
 
-  images : ObjectService[]= [
-    new ObjectService(1,"./../../assets/images/construction_traditionnelle2.jpg",
-    "./../../assets/images/icon_construction-traditionnelle.png",
-    "Construction Traditionnelle",
-    "Nous sommes spécialisées dans la conception et construction sur mesure de piscine traditionnelle. Nous réalisations vos envies, en assemblant solidité et longévité. Tous les ouvrages (constructions, réseaux, filtrations, étanchéités.) sont couverts par une responsabilité civile professionnelle et d'une assurance décennale. Avec une forte expérience dans le domaine de la piscine, nous sauront vous écoutez, vous conseillé, et vous accompagné dans votre projet de construction. Nous saurons également vous accompagner dans le choix technique de votre projet et également dans le choix esthétique de votre revêtement afin que. Cela correspond à vos attentes. Chaque projet et unique, c'est pour cela que nous étudierons vigoureusement le type de terrain, vos envies et votre budget."
-    ),
-    new ObjectService(2,
-      "./../../assets/images/renovation_etancheite.jpg",
-      "./../../assets/images/icon_renovation-etancheite.png",
-      "Rénovation et Étanchéité",
-      "Vous envisagez de rénover votre ancienne piscine, notre équipe expérimentée excelle dans la rénovation complète. Nous tous les travaux de rénovation de votre piscine telle que le changement et l'amélioration des pièces a sceller, des réseaux (canalisation). L'installation d'une nouvelle membrane d'étanchéité (PVC armé), le changement de vos margelles et pour finir une remise à la norme de votre Système de sécurité. Travaillant avec sérénité nous mettons en œuvre un diagnostic complet avant travaux. Nous redonnons vie à votre piscine, prolongeant ainsi sa durée tout en lui insufflant une touche contemporaine."
-    )]
+
 
   constructor(private el: ElementRef, private router : Router)  {}
   
