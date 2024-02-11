c25c489d6a61d6278201feceefc02373bd9aab14
raki <mohammed.raki26@gmail.com>
2024-02-11 16:49:21 +0100

    page learn-french

--- src/app/learn-arabic/learn-arabic.component.ts
+++ src/app/learn-arabic/learn-arabic.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OngletService } from '../services/onglet.service';
 
 @Component({
   selector: 'app-learn-arabic',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './learn-arabic.component.css'
 })
 export class LearnArabicComponent {
-
+  selectedImageUrl: string | null = null;
+  
+  constructor(private ongletService : OngletService){
+    
+  }
+  updateOnglet() {
+    // Mettez à jour l'objet Onglet en utilisant le service
+    this.ongletService.cacherLesOnglets();
+  }
 }
