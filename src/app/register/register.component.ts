4708333729d6c0fafaee38049f8420d559800b65
raki <mohammed.raki26@gmail.com>
2024-02-28 11:22:56 +0100

    create account

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,30 +1,59 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [NgClass, ReactiveFormsModule],
+  imports: [NgClass, ReactiveFormsModule, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   login : boolean = false
-  registerForm!: FormGroup<any>;
+  registerForm!: FormGroup;
+  loginForm!: FormGroup;
+  submitted = false;
+  submittedLogin = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+
+  constructor(private formBuilder: FormBuilder,
+              private httpClient: HttpClient) { }
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+      firstName: ['', [Validators.required, Validators.minLength(1)]],
+      lastName: ['', [Validators.required, Validators.minLength(1)]],
+    });
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
+  get fRegister(): { [key: string]: AbstractControl } {
+    return this.registerForm.controls;
+  }
+  get fLogin(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+  onSubmitRegister() {
+    // Ici, vous pouvez gérer la soumission de votre formulaire
+    this.submitted = true
+    if(this.submitted){
+      return
+    }
+    console.log(this.registerForm.value);
+  }
 
-  onSubmit() {
+  onSubmitLogin() {
+    
+    this.submittedLogin = true
+    if(this.submittedLogin){
+      return
+    }
     // Ici, vous pouvez gérer la soumission de votre formulaire
     console.log(this.registerForm.value);
   }
